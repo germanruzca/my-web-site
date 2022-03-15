082e467f1a5ebee063817dd758f68abd84a74034
German Ruiz <43076503+germanruzca@users.noreply.github.com>
2022-03-15 12:57:30 -0600

    The file to import the data and read the information

---
+++ pages/[slug].js
@@ -0,0 +1,35 @@
+import { MDXRemote } from 'next-mdx-remote';
+import { getFileBySlug, getFiles } from '../lib/mdx';
+import MDXComponents from '../components/MDXComponents'
+import {Header} from '../components';
+
+export default function Post({ source, frontmatter}) {
+  return (
+    <div>
+      <Header/>
+      <MDXRemote {...source} components={MDXComponents}/>
+    </div>
+  )
+}
+
+export async function getStaticProps({ params }) {
+  const { source, frontmatter } = await getFileBySlug(params.slug);
+
+  return {
+    props: { source, frontmatter },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = await  getFiles();
+  const paths = posts.map((post) => ({
+    params: {
+      slug: post.replace(/\.mdx/, ""),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
