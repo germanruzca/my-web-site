8ed8a00fadd5ee1b308367f7f61de228170d3a71
germanruzca <germanruzca@gmail.com>
2022-04-24 19:50:58 -0500

    Add the icons and text to the clock and date

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,21 +1,33 @@
 import styles from '../styles/PostCard.module.css';
+import { IconCalendar, IconComment, IconClock } from "./icons";
+import { formatDate } from "../lib/format_date";
 import Link from 'next/link'
 import { IconArrow } from './icons';
+import readingTime from "reading-time";
 
 export const PostCard = ({post}) => {
   return (
-    <div className={styles.card}>
-      <div>
-        <h2>{post.title}</h2>
-        <p>{post.date}</p>
-      </div>
-      <div className={styles.arrow_icon}>
-        <Link key={post.slug} href={`/${post.slug}`} >
+    <div>
+      <Link key={post.slug} href={`/${post.slug}`} >
+        <div className={styles.card}>
           <div>
-            <IconArrow size={40}/>
+            <h2 className={styles.title_card}>{post.title}</h2>
+            <div className={styles.calendar}>
+              <IconCalendar/>
+              <p className={styles.icons}>{formatDate(post.date)}</p>
+            </div>
+            <div className={styles.calendar}>
+              <IconClock/>
+              <p className={styles.icons}>{parseInt((readingTime(post.content).minutes))} min</p>
+            </div>
           </div>
-        </Link>
-      </div>
+          <div className={styles.arrow_icon}>
+              <div>
+                  <IconArrow size={40}/>
+              </div>
+          </div>
+        </div>
+      </Link>
     </div>
   )
 }
